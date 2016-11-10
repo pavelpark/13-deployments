ed91051e47b766dc9b6d412a76e73d9720fd9fff
Pavel Parkhomey <pavelpark@aol.com>
2016-11-09 16:03:11 -0800

    update get requests

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,8 +4,12 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.get call
     $.when(
-    $.get('/github'),
-    $.get('/github')
+    $.get('/github/users/codefellows-seattle-301d14/repos', function(data) {
+      reposObj.allRepos = data;
+    }),
+    $.get('/github/users/pavelpark/followers', function(data) {
+      reposObj.followers = data;
+    })
     ).done(callback);
   };
 
